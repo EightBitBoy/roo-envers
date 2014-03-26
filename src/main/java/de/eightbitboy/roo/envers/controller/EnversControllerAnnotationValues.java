e2f923492a9fbb2827a0b04956c144e10b38505e
Stefan Haertel <haertel.dev@gmail.com>
2014-03-26 14:18:10 +0100

    add class for accession annotation values

---
+++ src/main/java/de/eightbitboy/roo/envers/controller/EnversControllerAnnotationValues.java
@@ -0,0 +1,15 @@
+package de.eightbitboy.roo.envers.controller;
+
+import org.springframework.roo.classpath.details.ClassOrInterfaceTypeDetails;
+import org.springframework.roo.classpath.details.MemberHoldingTypeDetails;
+import org.springframework.roo.classpath.details.annotations.populator.AbstractAnnotationValues;
+import org.springframework.roo.model.JavaType;
+
+public class EnversControllerAnnotationValues extends AbstractAnnotationValues {
+
+	protected EnversControllerAnnotationValues(final ClassOrInterfaceTypeDetails governorPhysicalTypeDetails,
+			JavaType annotationType) {
+		super(memberHoldingTypeDetails, annotationType);
+		// TODO Auto-generated constructor stub
+	}
+}
