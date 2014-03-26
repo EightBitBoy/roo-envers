698bd2d0ac761e1d72859680f8d1afad7fa5fcba
Stefan Haertel <haertel.dev@gmail.com>
2014-03-26 11:09:24 +0100

    implement controller generator classes

--- src/main/java/de/eightbitboy/roo/envers/controller/EnversControllerMetadata.java
+++ src/main/java/de/eightbitboy/roo/envers/controller/EnversControllerMetadata.java
@@ -1,5 +1,35 @@
 package de.eightbitboy.roo.envers.controller;
 
-public class EnversControllerMetadata {
+import org.springframework.roo.classpath.PhysicalTypeIdentifierNamingUtils;
+import org.springframework.roo.classpath.PhysicalTypeMetadata;
+import org.springframework.roo.classpath.itd.AbstractItdTypeDetailsProvidingMetadataItem;
+import org.springframework.roo.metadata.MetadataIdentificationUtils;
+import org.springframework.roo.model.JavaType;
+import org.springframework.roo.project.LogicalPath;
 
+public class EnversControllerMetadata extends AbstractItdTypeDetailsProvidingMetadataItem {
+	
+    private static final String PROVIDES_TYPE_STRING = EnversControllerMetadata.class.getName();
+    private static final String PROVIDES_TYPE = MetadataIdentificationUtils.create(PROVIDES_TYPE_STRING);
+
+    public static final String getMetadataIdentiferType() {
+        return PROVIDES_TYPE;
+    }
+    
+    public static final String createIdentifier(JavaType javaType, LogicalPath path) {
+        return PhysicalTypeIdentifierNamingUtils.createIdentifier(PROVIDES_TYPE_STRING, javaType, path);
+    }
+    
+    public static final JavaType getJavaType(String metadataIdentificationString) {
+        return PhysicalTypeIdentifierNamingUtils.getJavaType(PROVIDES_TYPE_STRING, metadataIdentificationString);
+    }
+
+    public static final LogicalPath getPath(String metadataIdentificationString) {
+        return PhysicalTypeIdentifierNamingUtils.getPath(PROVIDES_TYPE_STRING, metadataIdentificationString);
+    }
+	
+	protected EnversControllerMetadata(String identifier, JavaType aspectName, PhysicalTypeMetadata governorPhysicalTypeMetadata) {
+		super(identifier, aspectName, governorPhysicalTypeMetadata);
+		//TODO do important stuff here
+	}
 }
--- src/main/java/de/eightbitboy/roo/envers/controller/EnversControllerMetadataProvider.java
+++ src/main/java/de/eightbitboy/roo/envers/controller/EnversControllerMetadataProvider.java
@@ -1,5 +1,57 @@
 package de.eightbitboy.roo.envers.controller;
 
-public class EnversControllerMetadataProvider {
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.osgi.service.component.ComponentContext;
+import org.springframework.roo.addon.web.mvc.controller.details.WebMetadataService;
+import org.springframework.roo.classpath.PhysicalTypeIdentifier;
+import org.springframework.roo.classpath.PhysicalTypeMetadata;
+import org.springframework.roo.classpath.itd.AbstractItdMetadataProvider;
+import org.springframework.roo.classpath.itd.ItdTypeDetailsProvidingMetadataItem;
+import org.springframework.roo.model.JavaType;
+import org.springframework.roo.project.LogicalPath;
 
+@Component
+@Service
+public class EnversControllerMetadataProvider extends AbstractItdMetadataProvider {
+	@Reference WebMetadataService webMetadataService;
+
+    protected void activate(ComponentContext context) {
+        metadataDependencyRegistry.registerDependency(PhysicalTypeIdentifier.getMetadataIdentiferType(), getProvidesType());
+        addMetadataTrigger(new JavaType(RooEnversController.class.getName()));
+    }
+	
+    protected void deactivate(ComponentContext context) {
+        metadataDependencyRegistry.deregisterDependency(PhysicalTypeIdentifier.getMetadataIdentiferType(), getProvidesType());
+        removeMetadataTrigger(new JavaType(RooEnversController.class.getName()));    
+    }
+	
+    protected ItdTypeDetailsProvidingMetadataItem getMetadata(
+    		String metadataIdentificationString,
+    		JavaType aspectName,
+    		PhysicalTypeMetadata governorPhysicalTypeMetadata,
+    		String itdFilename) {
+        // Pass dependencies required by the metadata in through its constructor
+        return new EnversControllerMetadata(metadataIdentificationString, aspectName, governorPhysicalTypeMetadata);
+    }
+	
+    public String getItdUniquenessFilenameSuffix() {
+        return "EnversController";
+    }
+	
+	
+    protected String getGovernorPhysicalTypeIdentifier(String metadataIdentificationString) {
+        JavaType javaType = EnversControllerMetadata.getJavaType(metadataIdentificationString);
+        LogicalPath path = EnversControllerMetadata.getPath(metadataIdentificationString);
+        return PhysicalTypeIdentifier.createIdentifier(javaType, path);
+    }
+
+    protected String createLocalIdentifier(JavaType javaType, LogicalPath path) {
+        return EnversControllerMetadata.createIdentifier(javaType, path);
+    }
+
+    public String getProvidesType() {
+        return EnversControllerMetadata.getMetadataIdentiferType();
+    }
 }
